d6ed243d26d4b56abbfb2f961fba5a341e1a57ee
Pierre Janineh <janinehpierre@gmail.com>
2024-09-16 13:13:12 +0300

    Initial Commit

---
+++ ISK Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ISK Example
+//
+//  Created by Pierre Janineh on 16/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ISK Example/ISK_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISK_ExampleApp.swift
+//  ISK Example
+//
+//  Created by Pierre Janineh on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ISK_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
